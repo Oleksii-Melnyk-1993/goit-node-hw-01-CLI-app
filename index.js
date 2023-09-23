03a6a03b7d62a473d8e91ba8ee87c7ba662c1332
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-09-23 22:03:20 +0300

    add contact list and index js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log(`Hello Oleksiil Melnik`);
+import * as contactsService from "./contacts.js";
+import { Command } from "commander";
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contactsService.listContacts;
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeOneContact = await contactsService.removeContact(id);
+      console.log(removeOneContact);
+      break;
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
